f00865442137d45155fedb0141d1fc3503989681
gidzkidz5 <124338423+gidzkidz5@users.noreply.github.com>
2023-07-02 16:53:57 +1000

    final

--- src/pages/Earphones.jsx
+++ src/pages/Earphones.jsx
@@ -3,12 +3,15 @@ import CategoriesBar from "../components/CategoriesBar"
 import TitleCard from "../components/TitleCard"
 import AboutAudiophile from "../components/AboutAudioPhile"
 
+
 export default function Earphones() {
     return (
         <>
+
         <TitleCard
             title = "earphones"
         />
+     
         <ProductList
             category = "earphones"
         />
